08b41d0c9cb0a1f4eb5154560ecbcbebb5212f9c
jkordonski <jkordonski97@gmail.com>
2020-11-19 19:12:58 +0000

    v2

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -6,24 +6,21 @@ unsigned int transportujFlota(unsigned int towar)
 {  
     int zaglowce = 0;
 
-    if(towar = 0)
+    if(towar == 0)
     {return 0;}
     else
     {
-    for (int ilosctowaru = 0; ilosctowaru <= towar;)
+      int ilosctowaru =0;
+    while (ilosctowaru < towar;)
     {
     Stocznia stocznia{};
-    Statek* s1 = stocznia{};
-    towar = towar + s1.transportuj();
+    Statek* s1 = stocznia();
+    towar = towar + s1->transportuj();
 
     if (s1 == dynamic_cast<Zaglowiec*>(s1))
     {zaglowce++}
-    else{}
-
     delete s1;
     }
-    }
-
-
     return zaglowce;
+}
 }
