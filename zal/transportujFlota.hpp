40e73cac2aecbb21e752631331afc72b58e74e75
jkordonski <jkordonski97@gmail.com>
2020-11-19 19:04:37 +0000

    v1

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -3,7 +3,27 @@
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
-{
-    // Twoja implementacja tutaj
-    return 0;
+{  
+    int zaglowce = 0;
+
+    if(towar = 0)
+    {return 0;}
+    else
+    {
+    for (int ilosctowaru = 0; ilosctowaru <= towar;)
+    {
+    Stocznia stocznia{};
+    Statek* s1 = stocznia{};
+    towar = towar + s1.transportuj();
+
+    if (s1 == dynamic_cast<Zaglowiec*>(s1))
+    {zaglowce++}
+    else{}
+
+    delete s1;
+    }
+    }
+
+
+    return zaglowce;
 }
